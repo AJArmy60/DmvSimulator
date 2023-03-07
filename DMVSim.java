645d1c97db007afda74222579fbc73a8ed16b0a3
AJArmy60 <aidanjarmellino@gmail.com>
2023-03-07 16:18:24 -0500

    100 to 200

--- DMVSim.java
+++ DMVSim.java
@@ -7,11 +7,11 @@ public class DMVSim
         System.out.println("Welcome to the DMV!");
  
         Random random = new Random();
-        int number = random.nextInt(100);
+        int number = random.nextInt(201);
         System.out.println("Here is your number: " + number);
         System.out.println("Please wait in line.");
        
-        for(int count = number; count < 100; count++)
+        for(int count = number; count < 200; count++)
         {
             System.out.println(count + 1);
         }
